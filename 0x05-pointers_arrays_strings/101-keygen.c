053c1f2f230d5c9b188ac7ea6b74d94267dac310
Faixybabe02 <imuetinyanobasuyi0022@gmail.com>
2023-02-27 13:44:11 -0800

    101-keygen.c

--- 0x05-pointers_arrays_strings/101-keygen.c
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -11,19 +11,18 @@ int main(void)
 {
 int pass[100];
 int i, sum, n;
-sum = 0;        
-srand(time(NULL));
+sum = 0;   srand(time(NULL));
 for (i = 0; i < 100; i++)
 {
 pass[i] = rand() % 78;
 sum += (pass[i] + '0');
 putchar(pass[i] + '0');
-if ((2772 - sum) - '0' < 78)
+if ((2772 - sum) -'0' < 78)
 {
-n = 2772 - sum -'0';
+n = 2772 - sum - '0';
 sum += n;
 putchar(n + '0');
 break;   }
 }
-return (0);
+return (0);      
 }
